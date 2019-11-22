e69db9ecc06b375a72abedf331f14717b49de685
Lucas Torti <luctort@outlook.com>
2019-11-21 19:18:02 -0800

    Added numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#numbers.sh
+#Lucas Torti
+#With help from Patrick Hooverson
+
+echo -n "Please enter a positive integer: "; read input
+
+echo i
+
+i=1
+while [ $i -le $input ]
+do
+	if [ $((i%2)) -eq 0 ]
+	echo
+	then
+			echo $i " even"
+		else 
+			echo $i " odd"
+		fi
+		i=$((i+1))	
+done
